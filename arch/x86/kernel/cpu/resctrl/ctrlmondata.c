1f22e176ae269c02074bb653c577951d9dba35a7
Reinette Chatre <reinette.chatre@intel.com>
2026-08-03 08:27:02 -0700

    x86/resctrl: SAMPLE: support for update of controls
    This is a SAMPLE implementation that uses a hardware control's
    msr_update instead of the existence of emulated control to navigate which
    controls to update.
    
    Introduce a new per-architecture .emulate_val() that returns the control
    value that emulated control needs to be programmed with.
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>
    ---
    Changes since RFC v1:
    - New patch.

--- arch/x86/kernel/cpu/resctrl/ctrlmondata.c
+++ arch/x86/kernel/cpu/resctrl/ctrlmondata.c
@@ -50,11 +50,49 @@ int resctrl_arch_update_one(struct rdt_resource *r, struct resctrl_ctrl *ctrl,
 	return 0;
 }
 
+/*
+ * Backing controls have the same number of domains and domain IDs map to
+ * same CPUs.
+ */
+static void program_backing_controls(struct rdt_resource *r, struct resctrl_ctrl *ctrl,
+				     struct rdt_ctrl_domain *d, u32 closid,
+				     struct msr_param *m)
+{
+	struct rdt_hw_ctrl_domain *em_hw_dom;
+	struct resctrl_staged_config *cfg;
+	struct resctrl_hw_ctrl *em_hw_ctrl;
+	struct resctrl_ctrl *em_ctrl;
+	struct rdt_ctrl_domain *em_d;
+	struct rdt_domain_hdr *hdr;
+	enum resctrl_conf_type t;
+	u32 idx;
+
+	list_for_each_entry(em_ctrl, &ctrl->emulated_by, entry) {
+		m->ctrl = em_ctrl;
+		hdr = resctrl_find_domain(&em_ctrl->domains, d->hdr.id, NULL);
+		em_d = container_of(hdr, struct rdt_ctrl_domain, hdr);
+		m->dom = em_d;
+		em_hw_dom = resctrl_to_arch_ctrl_dom(em_d);
+		em_hw_ctrl = resctrl_to_arch_ctrl(em_ctrl);
+		for (t = 0; t < CDP_NUM_TYPES; t++) {
+			cfg = &d->staged_config[t];
+			if (!cfg->have_new_ctrl)
+				continue;
+
+			idx = resctrl_get_config_index(closid, t);
+			em_hw_dom->ctrl_val[idx] = em_hw_ctrl->emulate_val(cfg->new_ctrl);
+		}
+
+		smp_call_function_any(&em_d->hdr.cpu_mask, rdt_ctrl_update, m, 1);
+	}
+}
+
 static void _resctrl_arch_update_domains(struct rdt_resource *r,
 					 struct resctrl_ctrl *ctrl, u32 closid)
 {
 	struct resctrl_staged_config *cfg;
 	struct rdt_hw_ctrl_domain *hw_dom;
+	struct resctrl_hw_ctrl *hw_ctrl;
 	struct msr_param msr_param;
 	struct rdt_ctrl_domain *d;
 	enum resctrl_conf_type t;
@@ -63,7 +101,12 @@ static void _resctrl_arch_update_domains(struct rdt_resource *r,
 	/* Walking ctrl->domains, ensure it can't race with cpuhp */
 	lockdep_assert_cpus_held();
 
+	/*
+	 * If control is emulated and mode is legacy, do not actually
+	 * update control, just stage the values of the emulating control
+	 */
 	msr_param.ctrl = ctrl;
+	hw_ctrl = resctrl_to_arch_ctrl(ctrl);
 	list_for_each_entry_rcu(d, &ctrl->domains, hdr.list, lockdep_is_cpus_held()) {
 		hw_dom = resctrl_to_arch_ctrl_dom(d);
 		msr_param.res = NULL;
@@ -73,8 +116,10 @@ static void _resctrl_arch_update_domains(struct rdt_resource *r,
 				continue;
 
 			idx = resctrl_get_config_index(closid, t);
-			if (cfg->new_ctrl == hw_dom->ctrl_val[idx])
+			if (cfg->new_ctrl == hw_dom->ctrl_val[idx]) {
+				cfg->have_new_ctrl = false;
 				continue;
+			}
 			hw_dom->ctrl_val[idx] = cfg->new_ctrl;
 
 			if (!msr_param.res) {
@@ -87,17 +132,26 @@ static void _resctrl_arch_update_domains(struct rdt_resource *r,
 				msr_param.high = max(msr_param.high, idx + 1);
 			}
 		}
-		if (msr_param.res)
+
+		if (!msr_param.res)
+			continue;
+
+		if (!hw_ctrl->msr_update)
+			program_backing_controls(r, ctrl, d, closid, &msr_param);
+		else
 			smp_call_function_any(&d->hdr.cpu_mask, rdt_ctrl_update, &msr_param, 1);
 	}
 }
 
-
+/*
+ * Avoid duplicate control configurations by only checking enabled controls
+ * for any staged configurations.
+ */
 int resctrl_arch_update_domains(struct rdt_resource *r, u32 closid)
 {
 	struct resctrl_ctrl *ctrl;
 
-	for_each_resource_ctrl(ctrl, r)
+	for_each_enabled_ctrl(ctrl, r)
 		_resctrl_arch_update_domains(r, ctrl, closid);
 
 	return 0;
